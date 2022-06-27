b5fa36db6a8725f50243186005805bbcd7d2341a
javiermorales <javiertoto-00@hotmail.com>
2022-06-26 19:27:58 -0700

    updates

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,64 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'PROJECT NAME',
+            message: 'What is the name of your project?',
+        },
+        {
+            type: 'input',
+            name: 'DESCRIPTION',
+            message: 'please provide a brief description of your project',
+        },
+        {
+            type: 'input',
+            name: 'TABLE OF CONTENTS',
+            message: '?',
+        },
+        {
+            type: 'input',
+            name: 'INSTALLATION',
+            message: '?',
+        },
+
+        {
+            type: 'input',
+            name: 'USAGE',
+            message: '?',
+        },
+        {
+            type: 'input',
+            name: 'LICENSE',
+            message: '?',
+        },
+        {
+            type: 'input',
+            name: 'CONTRIBUTION',
+            message: '?',
+        },
+        {
+            type: 'input',
+            name: 'TESTS',
+            message: '?',
+        },
+        {
+            type: 'input',
+            name: 'QUESTIONS',
+            message: '?',
+        },
+
+
+    ]);
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
