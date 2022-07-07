07114fa7d0ac6dc8de01a87ec2bd036eec6aeef9
javiermorales <javiertoto-00@hotmail.com>
2022-07-06 20:23:21 -0700

    something

--- Develop/Readme.md
+++ Develop/Readme.md
@@ -1,32 +1,32 @@
-# gvfywfe
+# README Generator
 
-
+ 
   ## Description
-fufuw
+\This application was created to generate a high-quality, professional README depending on a series of questions a user answears in the command lin eusing node.js
 
 ## Table of contents
-*[INSTALLATION](#INSTALLATION)
-*[USAGE](#USAGE)
-*[LICENSE](#LICENSE)
-*[CONTRIBUTION](#CONTRIBUTION)
-*[TESTS](#TESTS)
-*[questions](#questions)
+*[INSTALLATION](#installation)
+*[USAGE](#usage)
+*[LICENSE](#license)
+*[CONTRIBUTION](#contribution)
+*[TESTS](#tests)
+*[QUESTIONS](#questions)
 
 ## Installation
-fgo;4f
+The user must clone the repository from github and download node. This application also requires a file system and inquirer module.
 
 ## Usage
-fgu4fy4
+
 
 ## License
-This project is license under ethgugh3
+This project is license under tMIT
 
 ## Contribution
-ugfgugp
+The installation section should be followed
 
 ## Test
-gu4t
+npm test
 
 ## Questions
-if you have any questions in regards to this project, please contact mr at 44gu
-You can view more of my projects at https://github.com/4tg4gg
+if you have any questions in regards to this project, please contact me at javiertoto-00@hotmail.com
+You can view more of my projects at https://github.com/javitoto-00@hotmail.com
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,23 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseType = license.value;
+  let yourLicense = ""
+  if(licenseType ===  "MIT"){
+    yourLicense = '![License: MIT](https://img.shields.io/badge/LicenseLicense-${data.license}-yellow.svg)'
+  }else if (licenseType ===  "ISC"){
+    yourLicense = '![License: ISC](https://img.shields.io/badge/LicenseLicense-${data.license}-blue.svg)'
+  }else if (licenseType ===   "Apache"){
+    yourLicense = '![License:  Apache](https://img.shields.io/badge/LicenseLicense-${data.license}-red.svg)'
+  }else if (licenseType ===  "GNU"){
+    yourLicense = '![License: GNU](https://img.shields.io/badge/LicenseLicense-${data.license}-green.svg)'
+  }else if (licenseType === "Mozilla" ){
+    yourLicense = '![License: Mozilla](https://img.shields.io/badge/LicenseLicense-${data.license}-purple.svg)'
+  }else {
+    license.value = " "
+  }
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -14,7 +31,7 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
-
+ 
   ## Description
 ${data.description}
 
@@ -24,7 +41,7 @@ ${data.description}
 *[LICENSE](#license)
 *[CONTRIBUTION](#contribution)
 *[TESTS](#tests)
-*[questions](#questions)
+*[QUESTIONS](#questions)
 
 ## Installation
 ${data.installation}
