3bd12855c4358364a76f8ac0140e9b548ebbf82e
javiermorales <javiertoto-00@hotmail.com>
2022-08-17 20:36:22 -0700

    something

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -33,7 +33,7 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-${renderLicenseBadge(data.license)} 
+${renderLicenseBadge(data.licenseType)} 
 
  
   ## Description
